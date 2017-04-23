a66aa540aefcf18e62909d4d4e014c826b9138a9
李铭昕 <715557344@qq.com>
2017-04-23 20:00:06 +0800

    * 完成demo

--- app/index.js
+++ app/index.js
@@ -1,7 +1,9 @@
 const config = require('../config.js');
 const $ = require('jquery');
 $("#sub").click(function () {
-    var mobile = $("#mobile").val();
-    $("#output").append("<div>" + mobile + "</div>");
-    console.log(versions);
+    var name = $("#name").val();
+    var password = $("#password").val();
+    var html = $("<tr></tr>");
+    html.append("<td>" + name + "</td><td>" + password + "</td>")
+    $("#output").append(html);
 });
